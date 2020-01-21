f5e2b04a7786d4e87e48b6620ddd90065c85d576
Dayvid Montovanelli Moreira <dayvidmoreira7@gmail.com>
2020-01-21 04:41:10 -0300

    finalização da tela de chat
    O front ja está pronto, falta apenas o back e realizar a integração das partes

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -31,15 +31,21 @@ export default class Chat extends Component {
             <div className="chat-container">
                 { this.state.redirect !== '' ? <Redirect to={this.state.redirect} /> : null }
                 <Row>
-                    <Col sm={{size: 8, offset: 2}} xs={{size: 10, offset: 1}}>
+                    <Col sm={{size: 8, offset: 2}} xs={{size: 12}}>
                         <div className="chat-box">
                             <div className="room-area">
-                                <Button onClick={() => {this.handleExit()}} color="danger">X</Button>
+                                <Button className="close-button" onClick={() => {this.handleExit()}} color="danger">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+                                    <path d="M0 0h24v24H0z" fill="none"/></svg>
+                                </Button>
+                                <span>Sala de bate-papo geral</span>
+                                <div className="room-status"></div> {/* up || down */}
                             </div>
                             <div className="messages-area">
-                                <MessageBubbly me name="Dayvid" date={new Date()} text="Oi amor! Tudo bem ?" />
+                                {/* <MessageBubbly me name="Dayvid" date={new Date()} text="Oi amor! Tudo bem ?" />
                                 <MessageBubbly name="Bruna" date={new Date()} text="Tudo ótimo! E com você?" />
-                                <MessageBubbly me name="Dayvid" date={new Date()} text="Estou ótimo! Que bom que está tudo bem!" />
+                                <MessageBubbly me name="Dayvid" date={new Date()} text="Estou ótimo! Que bom que está tudo bem!" /> */}
                             </div>
                             <div className="user-area">
                                 <form>
